f7712df8c875a83f6a4195d8e065c9a149861bb2
VAIBHAV <vabby.bansal@gmail.com>
2016-07-17 22:53:34 +0530

    A simple Activity Fragment Implementation

--- app/src/main/java/tasku/apps/vaibhavbansal/tasku/MainInboxActivity.java
+++ app/src/main/java/tasku/apps/vaibhavbansal/tasku/MainInboxActivity.java
@@ -1,39 +1,35 @@
 package tasku.apps.vaibhavbansal.tasku;
 
-import android.support.v7.app.ActionBarActivity;
+
+
+
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
-public class MainInboxActivity extends ActionBarActivity {
+public class MainInboxActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_inbox);
-    }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_inbox, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        FragmentManager fm = getSupportFragmentManager();
+        //access the fragment container defined in the activity
+        Fragment fragment = fm.findFragmentById(R.id.id_fragment_container);
+        if(fragment == null){
+            fragment = new TaskListFragment();
+            fm.beginTransaction().add(R.id.id_fragment_container, fragment).commit();
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+
 }
---
+++ app/src/main/java/tasku/apps/vaibhavbansal/tasku/TaskListFragment.java
@@ -0,0 +1,31 @@
+package tasku.apps.vaibhavbansal.tasku;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+/**
+ * Created by VAIBHAV on 7/17/2016.
+ */
+public class TaskListFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_task_list, container, false);
+
+
+
+        return v;
+    }
+}
